329d2e2b8ce6ef5ab55e691ff4b6571cabbceb85
Leon Grösch <leongroesch@gmail.com>
2019-06-01 10:55:08 +0200

    New Try follows

--- binary_array.cpp
+++ binary_array.cpp
@@ -3,14 +3,15 @@
 using namespace std;
 
 binary_array::binary_array(int _size, unsigned int _row_count)
-: size(_size), byte_size(ceil(size/8.0)), array(byte_size), row_count(_row_count)
+: size(_size), byte_count(ceil(size/8.0)), array(byte_count), row_count(_row_count)
 {
-
+  row_size = size/row_count;
 }
 binary_array::binary_array(unsigned char* _array, unsigned int _size, unsigned int _row_count)
-: size(_size), byte_size(ceil(size/8.0)), array(byte_size), row_count(_row_count)
+: size(_size), byte_count(ceil(size/8.0)), array(byte_count), row_count(_row_count)
 {
-  for(int i = byte_size-1; i >= 0; i++)
+  row_size = size/row_count;
+  for(int i = 0; i < byte_count; i++)
   {
     array[i] = _array[i];
   }
@@ -18,9 +19,10 @@ binary_array::binary_array(unsigned char* _array, unsigned int _size, unsigned i
 }
 
 binary_array::binary_array(vector<int> _array, int _size, unsigned int _row_count)
-: size(_size), byte_size(ceil(size/8.0)), array(byte_size), row_count(_row_count)
+: size(_size), byte_count(ceil(size/8.0)), array(byte_count), row_count(_row_count)
 {
-  for(int i = _array.size()-1; i >=0 ; i--)
+  row_size = size/row_count;
+  for(unsigned int i = 0; i < _array.size() ; i++)
   {
     array[i] = ((char)_array[i]);
   }
@@ -44,10 +46,12 @@ void binary_array::print() const
 {
   int byte;
   int bit;
-  for(int i = size-1; i >= 0; i--)
+  for(int i = 0; i < size; i++)
   {
-    byte = floor(i/8);
-    bit = i%8;
+    if(i%row_size == 0)
+      cout<<"\n";
+    byte = i/8;
+    bit = 7-(i%8);
     cout<<((array[byte]>>bit)&0x01)<<" ";
   }
   cout<<"\n\n";
@@ -55,46 +59,79 @@ void binary_array::print() const
 
 void binary_array::print(char x) const
 {
-  for(int i = 8; i >= 0; i--)
+  for(int i = 7; i >= 0; i--)
     cout<<(x>>i&0x01)<<" ";
   cout<<"\n";
 }
 
-friend binary_array binary_array::xnor_mask(binary_array &mask, int midpoint_index) const
+binary_array binary_array::xnor_mask(binary_array &mask, int midpoint_index)
 {
-  if(size < tl_index + (mask.row_size * mask_col_count))
-    throw out_of_range("The mask does not fit over the matrix");
+  // if(size < midpoint_index + (mask.row_size * mask_col_count))
+  //   throw out_of_range("The mask does not fit over the matrix");
 
   //Calculate frequently used data
-  int kernel_offset = 0;
+  binary_array result(mask.size, mask.row_count);
   //Iterate ofter the maks columns
-  char arry_operand = 0;
-  char kernel_operand = 0;
-  for(int row = -(maks.row_count/2); row < mask.row_count/2  ; i++)
+  unsigned char array_operand = 0;
+  unsigned char kernel_operand = 0;
+  for(int row = -(mask.row_count/2); row <= (int)mask.row_count/2  ; row++)
   {
     int lh_matrix_idx = midpoint_index+(row*row_size) - mask.row_size/2;
-    //<<<To Do>>>
-    int lh_kernel_idx = row
+    int lh_kernel_idx = (row+mask.row_count/2)*mask.row_size;
     //Iterate over the bytes in one collumn
-    for(int byte = 0; byte < to_byte(mask.row_size)-1; byte++)
+    for(unsigned int byte = 0; byte < to_byte(mask.row_size)-1; byte++)
     {
       if((lh_matrix_idx%8) != 0)
-        array_operand = array[lh_matrix_idx/8+byte]<<(lh_matrix_idx%8) | array[lh_matrix_idx/8+byte+1] >> 8-(lh_matrix_idx%8);
+        array_operand = array[lh_matrix_idx/8+byte]<<(lh_matrix_idx%8) | array[lh_matrix_idx/8+byte+1] >> (8-lh_matrix_idx%8);
       else
         array_operand = array[lh_matrix_idx/8+byte];
-      kernel_operand = mask.array[byte]
-
-
+      kernel_operand = mask.array[to_byte(lh_kernel_idx)+byte];
+      cout<<"kernel_operand in for: ";
+      print(kernel_operand);
+      cout<<"array operand in for: ";
+      print(array_operand);
+      result[to_byte(lh_kernel_idx)+byte] = ~(array_operand^kernel_operand);
     }
     if(mask.row_size%8 == 0)
       array_operand = array[lh_matrix_idx/8+to_byte(mask.row_size)];
+    else if(mask.row_size > 8)
+      array_operand = (array[lh_matrix_idx/8 + mask.row_size/8]<<(8-mask.row_size%8)  | array[lh_matrix_idx/8+to_byte(mask.row_size)]);
     else
-      array_operand = (array[lh_matrix_idx/8 + mask.row_size/8]<<8-mask.row_size%8  | array[lh_matrix_idx/8+to_byte(mask.row_size)]);
+    {
+      array_operand = array[lh_matrix_idx/8 + mask.row_size/8];
+      array_operand = array_operand<<(lh_matrix_idx%8);
+      array_operand = array_operand>>(8-(mask.row_size%8));
+    }
+    kernel_operand = (mask.array[lh_kernel_idx/8+mask.row_size/8]<<lh_kernel_idx%8)>>(8-(mask.row_size%8));
+    cout<<"kernel_operand : ";
+    print(kernel_operand);
+    cout<<"array operand : ";
+    print(array_operand);
+    result[to_byte(lh_kernel_idx)+to_byte(mask.row_size)] = ~(array_operand^kernel_operand);
   }
 
   return result;
 }
 
+unsigned char binary_array::byte(int lh_idx, int rh_idx)
+{
+  if(lh_idx > rh_idx || rh_idx-lh_idx > 7|| rh_idx >= size)
+    throw out_of_range("Invalid argumnts");
+  unsigned char lh_byte = array[lh_idx/8];
+  unsigned char rh_byte = array[rh_idx/8];
+  if(rh_idx-lh_idx == 7)
+  {
+    lh_byte = lh_byte<<lh_idx%8;
+    rh_byte = rh_byte>>(8-lh_idx%8);
+  }
+  else
+  {
+    lh_byte = lh_byte<<lh_idx%8;
+    lh_byte &= 0xFF<<(rh_idx-lh_idx+1);
+    rh_byte = (rh_byte>>(7-rh_idx%8))<<((lh_idx%8)-(rh_idx%8+1));
+  }
+  return lh_byte | rh_byte;
+}
 
 
 
--- binary_array.h
+++ binary_array.h
@@ -8,18 +8,16 @@
 
 class binary_array
 {
-private:
-  std::vector<char> array;
+public:
   int size;
   int byte_count;
+  std::vector<unsigned char> array;
   unsigned int row_count;
   unsigned int row_size;
-  unsigned int col_count;
-  unsigned int col_size;
+
 
   inline unsigned int to_byte(int bit){ return ceil(bit/8.0);}
 
-public:
   binary_array(int _size, unsigned int _row_count);
   binary_array(unsigned char* _array, unsigned int _size, unsigned int _row_count);
   binary_array(std::vector<int>  _array, int _size, unsigned int _row_count);
@@ -30,8 +28,9 @@ public:
   void print() const;
   void print(char x) const;
   void push_back(char value) {array.push_back(value);} ;
-  friend binary_array xnor_mask(binary_array &mask, int rh_index) const;
-  char& operator[](int index){return array[index];}
+  binary_array xnor_mask(binary_array &mask, int midpoint_index);
+  unsigned char byte(int lh_idx, int rh_idx);
+  unsigned char& operator[](int index){return array[index];}
 };
 
 #endif
